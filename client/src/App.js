9d241597ab62a64382bd4df3e4508ad027333e3a
geovonnie vasquez <geovonnievasquez@gmail.com>
2021-03-24 10:53:41 -0500

    sass added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import react from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import sass file for styling
+import "./styles/app.scss"
+import LoginPage from "./components/LoginPage"
 
 function App() {
   return (
-   <div>
-     hello
-   </div>
+    <LoginPage />
   );
 }
 
--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Container} from "react-bootstrap"
 
 function LoginPage() {
     return (
-        <div>
+        <Container className="login-container">
+            <h2>Pick A Sport</h2>
             
-        </div>
+        </Container>
     )
 }
 

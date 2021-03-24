9973800631d2480ed702e49a21876c18e030b932
unknown <geovonnnievasquez@gmail.com>
2021-03-24 17:38:41 -0500

    login page styled not responsive yet

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Container} from "react-bootstrap"
+import LoginButton from "./LoginButton";
 
 function LoginPage() {
     return (
         <Container className="login-container">
             <h2>Pick A Sport</h2>
-            
+            <LoginButton />
         </Container>
     )
 }

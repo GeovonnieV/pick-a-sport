ae5229a0e06987010668ccc46471c1c54c6e162a
unknown <geovonnnievasquez@gmail.com>
2021-03-24 17:53:46 -0500

    is authenticated login

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import {Container} from "react-bootstrap"
+import React from 'react';
+import {Container} from "react-bootstrap";
 import LoginButton from "./LoginButton";
+import {useAuth0} from "@auth0/auth0-react";
 
 function LoginPage() {
+
+    const {isAuthenticated} = useAuth0(); 
+
     return (
+        // if user is not authenitcated show 
+        !isAuthenticated && (
         <Container className="login-container">
             <h2>Pick A Sport</h2>
             <LoginButton />
         </Container>
+        )
     )
-}
+};
 
 export default LoginPage
 
